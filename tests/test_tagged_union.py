9aa09448d380ea16a1f528be621e6211c6f108a1
Thomas Carotti <thomas@carotti.co.uk>
2019-07-01 20:54:39 +0100

    Add repr and equality tests

--- tests/test_tagged_union.py
+++ tests/test_tagged_union.py
@@ -7,14 +7,41 @@ def test_match_int():
 
     assert result == 1
 
+@tagged_union
+class A(object):
+    B = Self
+    C = int
+
+c = A.C(1)
+b = A.B(c)
+
 def test_tagged_union_subclass():
-    @tagged_union
-    class A(object):
-        B = Self
-        C = int
-
-    c = A.C(1)
-    b = A.B(c)
-
     assert isinstance(b, A)
     assert isinstance(c, A)
+
+def test_repr_default():
+    assert repr(c) == "A.C(1,)"
+    assert repr(b) == "A.B(A.C(1,),)"
+
+def test_eq_default():
+    assert c == A.C(1)
+    assert b == A.B(c)
+    assert b == A.B(A.C(1))
+
+@tagged_union
+class ReprUnion:
+    B = Self
+    C = int
+
+    def __repr__(self):
+        return match(self, {
+            A.B: lambda _: "BBB",
+            A.C: lambda _: "CCC",
+        })
+
+cr = ReprUnion.C(1)
+br = ReprUnion.B(cr)
+
+def test_repr_override():
+    assert repr(cr) == "CCC"
+    assert repr(br) == "BBB"
